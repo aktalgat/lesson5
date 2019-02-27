b018f24fad48651e37ec65bc273e34ef94f21ad5
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2019-02-27 20:54:59 +0600

    Initial commit

---
+++ src/main/java/kg/geektech/App.java
@@ -0,0 +1,16 @@
+package kg.geektech;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class App {
+
+    private static final Logger LOGGER = LogManager.getLogger(App.class.getName());
+
+    public static void main(String[] args) {
+        LOGGER.warn("Warn");
+        LOGGER.debug("Debug сообщение !!!");
+        LOGGER.info("Info сообщение !!!");
+        LOGGER.error("Error сообщение !!!", new NullPointerException("NullError"));
+    }
+}
